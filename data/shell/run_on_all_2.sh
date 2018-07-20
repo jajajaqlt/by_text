3d392af3d6be6e9896247ed9cdce52adc8ef03e3
jajaja <jajaja@jajajadeMac.local>
2018-07-20 02:32:56 -0500

    rename the output name in shell

--- data/shell/run_on_all_2.sh
+++ data/shell/run_on_all_2.sh
@@ -6,5 +6,5 @@ for filedir in output_jsons/*; do
     echo $filename
     filestem=$(echo $filename| cut -d'.' -f 1)
     echo $filestem
-    python3 apply_dictionaries_s2_json.py $filedir ready_to_use/$filestem.json lists/short-titles_0.9 lists/short-authors_5 lists/short-venues_50
+    python3 apply_dictionaries_s2_json.py $filedir ready_to_use/$filestem-ready.json lists/short-titles_0.9 lists/short-authors_5 lists/short-venues_50
 done
